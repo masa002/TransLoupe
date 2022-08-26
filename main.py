3b009f83f3de3b702d28ab04af8df896c2bdc061
masa002 <m.obara.sys21@morijyobi.ac.jp>
2022-08-26 12:10:23 +0900

    add comments

--- main.py
+++ main.py
@@ -34,8 +34,10 @@ def main():
     old_text = ''
     while True:
         try:
+            # get lang setting
             lang = langs[var.get()]
             for text in ocr(frame.winfo_x(), frame.winfo_y(), lang[0]):
+                # diff check
                 if text != old_text:
                     text = translate(text, lang[1])
                     old_text = text
@@ -50,9 +52,11 @@ def main():
         time.sleep(1)
 
 def window():
+    # add invisible label
     lbl = tk.Label(background='snow')
     lbl.place(x=0, y=0, width=300, height=30)
 
+    # add radio buttons
     radio1 = tk.Radiobutton(frame, text="JPN -> ENG", value=0, variable=var, font=font)
     radio1.place(x=10, y=90)
     radio2 = tk.Radiobutton(frame, text="ENG -> JPN", value=1, variable=var, font=font)
@@ -66,32 +70,37 @@ def window():
     radio6 = tk.Radiobutton(frame, text="THA -> ENG", value=5, variable=var, font=font)
     radio6.place(x=160, y=170)
 
+    # start main loop
     frame.mainloop()
 
 
 if __name__ == '__main__':
+    # translator setting
     translator = Translator()
 
+    # ocr settings
     pyocr.tesseract.TESSERACT_CMD = resourcePath('resources/Tesseract-OCR/tesseract.exe')
     tools = pyocr.get_available_tools()
 
+    # gui settings
     frame = tk.Tk()
     var = tk.IntVar()
     var.set(0)
     font = ('Helvetica', 12)
     iconfile = './resources/icon.ico'
-
     frame.iconbitmap(default=iconfile)
     frame.title("TransLoupe") 
     frame.geometry('300x250')
     frame.wm_attributes('-transparentcolor', 'snow')
     frame.resizable(0,0)
 
+    # add output label
     txt = tk.Entry()
     txt.place(x=0, y=30, width=300, height=30)
 
     langs = {0: ['jpn', 'en'], 1: ['eng', 'ja'], 2: ['jpn', 'th'], 3: ['tha', 'ja'], 4: ['eng', 'th'], 5: ['tha', 'en']}
 
+    # create thread and start
     thread = threading.Thread(target=main)
     thread.start()
     window()
